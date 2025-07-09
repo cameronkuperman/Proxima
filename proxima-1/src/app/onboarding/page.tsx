0c62a8353ba53bded0f5f010f68d2e3ea198a510
kwame <kwamebb36@gmail.com>
2025-07-09 19:46:15 -0400

    onboarding redirection/routing fixes

--- proxima-1/src/app/onboarding/page.tsx
+++ proxima-1/src/app/onboarding/page.tsx
@@ -1,10 +1,13 @@
 import OnboardingFlow from '@/components/OnboardingFlow';
 import AuthGuard from '@/components/AuthGuard';
+import OnboardingGuard from '@/components/OnboardingGuard';
 
 export default function OnboardingPage() {
   return (
     <AuthGuard requireAuth={true}>
-      <OnboardingFlow />
+      <OnboardingGuard>
+        <OnboardingFlow />
+      </OnboardingGuard>
     </AuthGuard>
   );
 } 
